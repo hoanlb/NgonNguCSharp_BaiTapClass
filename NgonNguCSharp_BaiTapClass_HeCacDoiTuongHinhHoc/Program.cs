eeb9bf97faabfeda9cc822ba606579895c51a0d3
Lâm Bình Hoàn <hoanlb1988@gmail.com>
2024-02-18 10:43:18 +0700

    He cac doi tuong hinh hoc

---
+++ NgonNguCSharp_BaiTapClass_HeCacDoiTuongHinhHoc/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Models;
+
+namespace NgonNguCSharp_BaiTapClass_HeCacDoiTuongHinhHoc
+{
+    // static void Main(string[] args)
+    // {
+         
+    // }
+
+    public class AllTest
+    {
+        static void Main(string[] args)
+        {
+            // Shape
+            Shape shape = new Shape();
+            Console.WriteLine(shape);
+
+            shape = new Shape("red", false);
+            Console.WriteLine(shape);
+            Console.WriteLine("Hello World!");
+
+            //Circle
+            Circle circle = new Circle();
+            Console.WriteLine(circle);
+
+            circle = new Circle(3.5);
+            Console.WriteLine(circle);
+
+
+            circle = new Circle(3.5, "indigo", false);
+            Console.WriteLine(circle);
+
+            // Rectangle
+            Rectangle rectangle = new Rectangle();
+            Console.WriteLine(rectangle);
+
+            rectangle = new Rectangle(2.3, 5.8);
+            Console.WriteLine(rectangle);
+
+            rectangle = new Rectangle(2.5, 3.8, "orange", true);
+            Console.WriteLine(rectangle);
+
+            //Square
+            Square square = new Square();
+            Console.WriteLine(square);
+
+            square = new Square(2.3);
+            Console.WriteLine(square);
+
+            square = new Square(5.8, "yellow", true);
+            Console.WriteLine(square);
+
+            // ReadKey
+            Console.ReadKey();
+        }
+    }
+}
